a24d317847fc98f31b30fc657d0a0a9c84581676
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-11-20 23:38:57 -0800

    Ping API and send response to LED marquee

---
+++ message.pl
@@ -0,0 +1,9 @@
+use Device::MiniLED;
+my $sign=Device::MiniLED->new(devicetype => "sign");
+
+$sign->addMsg(
+  data => `curl <web-address> --retry 5 --retry-delay 4`,
+  effect => "scroll",
+  speed => 2
+);
+$sign->send(device => "/dev/ttyUSB0");
